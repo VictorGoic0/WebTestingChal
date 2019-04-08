00edf5e390fb5b0071fec01b11f4e6f83824df77
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-08 17:47:44 -0400

    Wrote out functionality for enhancer.succeed, wrote out tests for all 3 items which checked out

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,20 +1,20 @@
 const enhancer = require("./enhancer.js");
 
 const item1 = {
-  name: "item 1",
+  name: "Iron Sword",
   enhancement: 12,
   durability: 50
 };
 
 const item2 = {
-  name: "item 2",
-  enhancement: 18,
+  name: "Golden Scythe",
+  enhancement: 4,
   durability: 70
 };
 
 const item3 = {
-  name: "item 3",
-  enhancement: 4,
+  name: "Excalibur",
+  enhancement: 20,
   durability: 100
 };
 
@@ -23,5 +23,23 @@ test("Should return an item with the durability restored to 100.", function() {
     name: item1.name,
     enhancement: item1.enhancement,
     durability: 100
+  });
+});
+
+test("Should return an item with the enhancementt 1 point higher, unless it is 20", function() {
+  expect(enhancer.succeed(item1)).toEqual({
+    name: item1.name,
+    enhancement: 13,
+    durability: item1.durability
+  });
+  expect(enhancer.succeed(item2)).toEqual({
+    name: item2.name,
+    enhancement: 5,
+    durability: item2.durability
+  });
+  expect(enhancer.succeed(item3)).toEqual({
+    name: item3.name,
+    enhancement: 20,
+    durability: item3.durability
   });
 });
