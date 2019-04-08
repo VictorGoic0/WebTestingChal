1557536f2e1447f5cc24661f533713a16ab83128
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-08 17:41:02 -0400

    Wrote a basic repair function and repair function test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,27 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+
+const item1 = {
+  name: "item 1",
+  enhancement: 12,
+  durability: 50
+};
+
+const item2 = {
+  name: "item 2",
+  enhancement: 18,
+  durability: 70
+};
+
+const item3 = {
+  name: "item 3",
+  enhancement: 4,
+  durability: 100
+};
+
+test("Should return an item with the durability restored to 100.", function() {
+  expect(enhancer.repair(item1)).toEqual({
+    name: item1.name,
+    enhancement: item1.enhancement,
+    durability: 100
+  });
+});
